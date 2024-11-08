dd7a214990904e8c0bdecb2f022c38e1d82bc875
Anna Freiman <annafrei924@gmail.com>
2024-11-08 14:16:52 -0500

    checkstyles

---
+++ src/test/java/bfk/brickbreaker/ControllerTest.java
@@ -0,0 +1,12 @@
+package bfk.brickbreaker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ControllerTest {
+    @Test
+    public void hitPaddle() {
+    }
+}
