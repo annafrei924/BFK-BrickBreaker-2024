8f6f7ba8e419a11dc2ac6b35c7815c7c0b50897e
FIRST_NAME LAST_NAME <lbennett9@student.touro.edu>
2024-11-12 13:36:10 -0500

    added test for when ball hits wall

--- src/test/java/bfk/brickbreaker/ControllerTest.java
+++ src/test/java/bfk/brickbreaker/ControllerTest.java
@@ -39,6 +39,37 @@ public class ControllerTest {
     }
 
     @Test
+    public void hitWall() {
+        // Given
+        Ball ball = new Ball(145, 10, 260, 680, 20, 20);
+        Paddle paddle = new Paddle(250, 690, 100, 20);
+        BBComponent view = mock(BBComponent.class);
+
+        Brick[] bricks = new Brick[20];
+
+        BBController controller = new BBController(ball, paddle, view, bricks);
+
+        // when
+        ball.setAngle(45);
+        controller.bounce(Direction.TOP);
+        // then
+        assertEquals(315, ball.getAngle());
+
+        // when
+        ball.setAngle(135);
+        controller.bounce(Direction.LEFT);
+        // then
+        assertEquals(45, ball.getAngle());
+
+        // when
+        ball.setAngle(45);
+        controller.bounce(Direction.RIGHT);
+        // then
+        assertEquals(135, ball.getAngle());
+    }
+
+
+    @Test
     public void ballMoves() {
         // given
         Ball ball = new Ball(145, 10, 260, 680, 20, 20);
