b6b81630d53ac6bb1562ee37a0e75ee7912fc488
Anna Freiman <annafrei924@gmail.com>
2024-11-18 19:53:55 -0500

    fixing checkstyles

--- src/main/java/bfk/brickbreaker/BBController.java
+++ src/main/java/bfk/brickbreaker/BBController.java
@@ -159,7 +159,9 @@ public class BBController{
         return paddle;
     }
 
-    public Ball getBall() { return ball; }
+    public Ball getBall() {
+        return ball;
+    }
 
     public int getTicks() {
         return tickCounter;
